16f7fbbd75669ab106a58e68be3db2e198ad8d1b
unsoul7 <disketteit@gmail.com>
2024-11-20 10:16:25 +0530

    started

---
+++ src/pages/Signup.js
@@ -0,0 +1,15 @@
+import '../styles/pagestyle.css'
+import lock from '../assets/lock.svg'
+
+export const Signup = () => {
+    return(
+        <div className="form-container">
+            <div></div>
+            <input type="text" />
+            <div></div>
+            <input type="text" />
+            <div></div>
+            
+        </div>
+    )
+}
